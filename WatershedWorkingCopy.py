3241763b51b394fcaca3a95e6b4c2ddd15a5ebee
Muerdra <56932817+Muerdra@users.noreply.github.com>
2019-11-25 09:43:30 -0500

    Update WatershedWorkingCopy.py

--- WatershedWorkingCopy.py
+++ WatershedWorkingCopy.py
@@ -36,71 +36,75 @@ RunoffRatio_Watershed = MVWRunoffRatio()
 
 import csv
 
-with open("Discharge2008.csv") as fin:
+def main():
+    with open("Discharge2008.csv") as fin:
 
-    headerline = fin.next()
-    Jan = 0
-    Feb = 0
-    Mar = 0
-    Apr = 0
-    May = 0
-    Jun = 0
-    Jul = 0
-    Aug = 0
-    Sep = 0
-    Oct = 0
-    Nov = 0
-    Dec = 0
+        fin.next()
+        Jan = 0
+        Feb = 0
+        Mar = 0
+        Apr = 0
+        May = 0
+        Jun = 0
+        Jul = 0
+        Aug = 0
+        Sep = 0
+        Oct = 0
+        Nov = 0
+        Dec = 0
 
-    for row in csv.reader(fin):
-        runoffJan = float(row[1])
-        runoffJan = (runoffJan*1000*24*3600)/(2620000000)
+        for row in csv.reader(fin):
+            runoffJan = float(row[1])
+            runoffJan = (runoffJan*1000*24*3600)/(2620000000)
 
-        runoffFeb = float(row[2])
-        runoffFeb = (runoffFeb*1000*24*3600)/(2620000000)
+            runoffFeb = float(row[2])
+            runoffFeb = (runoffFeb*1000*24*3600)/(2620000000)
 
-        runoffMar = float(row[3])
-        runoffMar = (runoffMar*1000*24*3600)/(2620000000)
+            runoffMar = float(row[3])
+            runoffMar = (runoffMar*1000*24*3600)/(2620000000)
 
-        runoffApr = float(row[4])
-        runoffApr = (runoffApr*1000*24*3600)/(2620000000)
+            runoffApr = float(row[4])
+            runoffApr = (runoffApr*1000*24*3600)/(2620000000)
 
-        runoffMay = float(row[5])
-        runoffMay = (runoffMay*1000*24*3600)/(2620000000)
+            runoffMay = float(row[5])
+            runoffMay = (runoffMay*1000*24*3600)/(2620000000)
 
-        runoffJun = float(row[6])
-        runoffJun = (runoffJun*1000*24*3600)/(2620000000)
+            runoffJun = float(row[6])
+            runoffJun = (runoffJun*1000*24*3600)/(2620000000)
 
-        runoffJul = float(row[7])
-        runoffJul = (runoffJul*1000*24*3600)/(2620000000)
+            runoffJul = float(row[7])
+            runoffJul = (runoffJul*1000*24*3600)/(2620000000)
 
-        runoffAug = float(row[8])
-        runoffAug = (runoffAug*1000*24*3600)/(2620000000)
+            runoffAug = float(row[8])
+            runoffAug = (runoffAug*1000*24*3600)/(2620000000)
 
-        runoffSep = float(row[9])
-        runoffSep = (runoffSep*1000*24*3600)/(2620000000)
+            runoffSep = float(row[9])
+            runoffSep = (runoffSep*1000*24*3600)/(2620000000)
 
-        runoffOct = float(row[10])
-        runoffOct = (runoffOct*1000*24*3600)/(2620000000)   
-        
-        runoffNov = float(row[11])
-        runoffNov = (runoffNov*1000*24*3600)/(2620000000) 
+            runoffOct = float(row[10])
+            runoffOct = (runoffOct*1000*24*3600)/(2620000000)   
+            
+            runoffNov = float(row[11])
+            runoffNov = (runoffNov*1000*24*3600)/(2620000000) 
 
-        runoffDec = float(row[12])
-        runoffDec = (runoffDec*1000*24*3600)/(2620000000)
+            runoffDec = float(row[12])
+            runoffDec = (runoffDec*1000*24*3600)/(2620000000)
 
-        Jan += float(runoffJan)
-        Feb += float(runoffFeb)
-        Mar += float(runoffMar)
-        Apr += float(runoffApr)
-        May += float(runoffMay)
-        Jun += float(runoffJun)
-        Jul += float(runoffJul)
-        Aug += float(runoffAug)
-        Sep += float(runoffSep)
-        Oct += float(runoffOct)
-        Nov += float(runoffNov)
-        Dec += float(runoffDec)
-        total = Jan + Feb + Mar + Apr + May + Jun + Jul + Aug + Sep + Oct + Nov + Dec 
+            Jan += float(runoffJan)
+            Feb += float(runoffFeb)
+            Mar += float(runoffMar)
+            Apr += float(runoffApr)
+            May += float(runoffMay)
+            Jun += float(runoffJun)
+            Jul += float(runoffJul)
+            Aug += float(runoffAug)
+            Sep += float(runoffSep)
+            Oct += float(runoffOct)
+            Nov += float(runoffNov)
+            Dec += float(runoffDec)
+            total = Jan + Feb + Mar + Apr + May + Jun + Jul + Aug + Sep + Oct + Nov + Dec 
     
     print total
+    
+if __name__ == '__main__':
+    main()
